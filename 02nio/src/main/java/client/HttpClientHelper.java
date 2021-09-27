f8c98388f83a7a1c34c03b1fbe07ccaae9776ec3
wangjian <wjchenge@qq.com>
2021-09-27 15:54:33 +0800

    第二周作业补充

---
+++ 02nio/src/main/java/client/HttpClientHelper.java
@@ -0,0 +1,53 @@
+package client;
+
+import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @Author wj
+ * @Date 2021/9/27 14:55
+ */
+public class HttpClientHelper {
+
+    public static void main(final String[] args) throws Exception {
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            final HttpGet httpget = new HttpGet("http://127.0.0.1:8801");
+
+            System.out.println("Executing request " + httpget.getMethod() + " " + httpget.getUri());
+
+            // Create a custom response handler
+            final HttpClientResponseHandler<String> responseHandler = new HttpClientResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final ClassicHttpResponse response) throws IOException {
+                    final int status = response.getCode();
+                    if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
+                        final HttpEntity entity = response.getEntity();
+                        try {
+                            return entity != null ? EntityUtils.toString(entity) : null;
+                        } catch (final ParseException ex) {
+                            throw new ClientProtocolException(ex);
+                        }
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            final String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,6 +12,23 @@ java进阶训练营作业
 
 [作业2](02nio/作业2.md)
 
+### 作业4 （必做）
+
+[作业4](02nio/作业4.md)
+
+### 作业6 （必做）写一段代码，使用 HttpClient 或 OkHttp 访问  http://localhost:8801 ，代码提交到 GitHub
+
+[HttpClientHelper.java](02nio/src/main/java/client/HttpClientHelper.java)
+
+运行结果
+
+    SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
+    SLF4J: Defaulting to no-operation (NOP) logger implementation
+    SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
+    Executing request GET http://127.0.0.1:8801/
+    ----------------------------------------
+    hello,nio1
+
 
 ## week01作业
 
